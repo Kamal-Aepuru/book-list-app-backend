d982f1926f12e9a25c56cdb358190249635d0141
Kamal-Aepuru <kamalaepuru@gmail.com>
2025-01-22 17:50:36 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const { initializeDatabase } = require("./db/db.connection");
+const { BooksList } = require("./models/books.model");
+const booksData = require("./books.json");
+
+app.use(cors());
+app.use(express.json());
+
+initializeDatabase();
+
+const seedData = async () => {
+  try {
+    await BooksList.insertMany(booksData);
+    console.log("Books inserted successfully.");
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+// seedData();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const allbooks = await BooksList.find();
+    res.json(allbooks);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/books", async (req, res) => {
+  const { bookName, author, genre } = req.body;
+
+  try {
+    const bookData = new BooksList({ bookName, author, genre });
+    await bookData.save();
+    res.status(201).json(bookData);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  const bookId = req.params.id;
+
+  try {
+    const deletedBook = await BooksList.findByIdAndRemove(bookId);
+
+    if (!deletedBook) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.status(200).json({
+      message: "Book deleted successfully",
+      book: deletedBook,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
